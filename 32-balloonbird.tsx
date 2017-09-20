68eb32108125b6868b37128d20ae26caa85dbb86
wanwan1910 <wanwan1910@gmail.com>
2017-09-20 21:45:56 +0700

    Add files via upload

---
+++ 32-balloonbird.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tile-chapter1-32" tilewidth="32" tileheight="32" spacing="2" margin="2" tilecount="12" columns="4">
+ <image source="tile-chapter1-hd.png" width="138" height="104"/>
+ <tile id="4">
+  <properties>
+   <property name="tile" type="int" value="1"/>
+  </properties>
+  <objectgroup draworder="index">
+   <properties>
+    <property name="tile" type="int" value="1"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="tile" type="int" value="2"/>
+  </properties>
+  <objectgroup draworder="index">
+   <properties>
+    <property name="tile" type="int" value="2"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="tile" type="int" value="3"/>
+  </properties>
+  <objectgroup draworder="index">
+   <properties>
+    <property name="tile" type="int" value="3"/>
+   </properties>
+  </objectgroup>
+ </tile>
+</tileset>
